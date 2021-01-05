418cd17e5dcf8b314814f240d2fceb0c87c0ac11
Ishan Goel <igoel.mail@gmail.com>
2021-01-05 19:06:39 +0400

    Quote arguments in shell command not found scripts

--- shell/tii_on_command_not_found.fish
+++ shell/tii_on_command_not_found.fish
@@ -2,6 +2,6 @@
 function tii_on_command_not_found --on-event fish_command_not_found
     if status --is-interactive # make sure a human is there to agree or disagree
         echo -e "Searching for command with Tii"
-        tii $argv[1]
+        tii "$argv[1]"
     end
 end
