89d2f3e16a190c610048d123c35a13d6f16a328b
Ishan Goel <igoel.mail@gmail.com>
2021-01-05 19:06:39 +0400

    Quote arguments in shell command not found scripts

--- shell/tii_on_command_not_found.sh
+++ shell/tii_on_command_not_found.sh
@@ -2,12 +2,12 @@
 if [ "$ZSH_VERSION" ] && [ -t 0 ]; then # is zsh and is interactive
   command_not_found_handler() {
     echo "Searching for command with Tii"
-    tii $1
+    tii "$1"
   }
 elif [ "$BASH_VERSION" ] && [ -t 0 ]; then # is bash and is interactive
   command_not_found_handle() {
     echo "Searching for command with Tii"
-    tii $1
+    tii "$1"
   }
 else
   echo -e "This is not bash or zsh. Please run $0 only in these shells."
--- tii.go
+++ tii.go
@@ -42,7 +42,7 @@ automatically trigger it. The name Tii is an acronym for "Then Install It".`
 	underline               = color.New(color.Underline).SprintFunc()
 	disablePrompts          = os.Getenv("TII_DISABLE_INTERACTIVE") == "true"
 	autoInstallExactMatches = os.Getenv("TII_AUTO_INSTALL_EXACT_MATCHES") == "true"
-	version                 = "Tii v1.0.3"
+	version                 = "Tii v1.0.4"
 )
 
 func main() {
