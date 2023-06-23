cbc43b8a4577d446187023299c4e07c0ee36dcbd
Ishan Goel <igoel.mail@gmail.com>
2023-06-23 14:58:07 -0700

    Use Homebrew JSON API + Caching. Adds package descriptions and better fuzzy matching.

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module tii
 go 1.15
 
 require (
-	github.com/fatih/color v1.13.0
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	github.com/fatih/color v1.15.0
+	github.com/lithammer/fuzzysearch v1.1.8
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
 )
--- tii.go
+++ tii.go
@@ -3,15 +3,16 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"runtime"
+	"sort"
 	"strconv"
-	"strings"
 
 	"github.com/fatih/color"
+	"github.com/lithammer/fuzzysearch/fuzzy"
 )
 
 var (
@@ -20,11 +21,11 @@ var (
 
 On most GNU/Linux systems, when a command is not found, a message showing what
 can be run to install the command is printed. However, macOS does not have
-this. This program adds a similar function with support for macOS (only for
-macOS, as of now). Instead of simply printing the command, Tii also offers to
-run it for you.
+this. This program adds a similar function with support for macOS using
+Homebrew. Instead of simply printing the best matches, Tii shows package 
+descriptions and also offers to run an install command for you.
 
-Usage: tii [--help/-h | --version/-v | <command>]
+Usage: tii [--help/-h | --version/-v | --refresh-cache/-r | <command>]
 
 Examples:
    tii fish
@@ -32,31 +33,48 @@ Examples:
    tii --help
 
 Environment:
-	TII_DISABLE_INTERACTIVE: If this variable is set to "true", Tii will
-		disable interactive output (prompting for confirmation) and not install
-		any packages.
-	TII_AUTO_INSTALL_EXACT_MATCHES: If this variable is set to "true", Tii will
-		automatically install exact matches without prompting for confirmation
+   TII_DISABLE_INTERACTIVE: If this variable is set to "true", Tii will
+      disable interactive output (prompting for confirmation) and not install
+      any packages.
+   TII_AUTO_INSTALL_EXACT_MATCHES: If this variable is set to "true", Tii will
+      automatically install exact matches without prompting for confirmation
+
+Files:
+   $XDG_DATA_HOME/tii: used to cache package list info. If $XDG_DATA_HOME is
+      not set, ~/.local/share is used instead. Refresh the cache using the 
+      --refresh-cache option.
 
 If Tii was installed correctly, using commands which are not found will
-automatically trigger it. The name Tii is an acronym for "Then Install It".`
-	prefix                  = "" // set in init()
+automatically trigger it. The name Tii is an acronym for "Then Install It".
+
+See source and report bugs at github.com/quackduck/tii`
+	//prefix                  = "" // set in init()
 	underline               = color.New(color.Underline).SprintFunc()
 	disablePrompts          = os.Getenv("TII_DISABLE_INTERACTIVE") == "true" //nolint // complains about using the literal string "true" 3 times
 	autoInstallExactMatches = os.Getenv("TII_AUTO_INSTALL_EXACT_MATCHES") == "true"
 	version                 = "development" // This will be set at build time using ldflags: go build -ldflags="-s -w -X main.version=$(git describe --tags --abbrev=0)"
+	formulaURL              = "https://formulae.brew.sh/api/formula.json"
+	caskURL                 = "https://formulae.brew.sh/api/cask.json"
+	dataDir                 = os.Getenv("XDG_DATA_HOME")
+	dataFile                = "pkginfo.json"
 )
 
-func init() {
-	// on M1, the formula location is different
-	if runtime.GOARCH == "arm64" {
-		prefix = "/opt/homebrew/Library/Taps/homebrew"
-	} else {
-		prefix = "/usr/local/Homebrew/Library/Taps/homebrew"
-	}
+type Formula struct {
+	Name string `json:"name"`
+	//FullName string `json:"full_name"`
+	Desc string `json:"desc"`
+}
+
+type Cask struct {
+	Name string `json:"token"`
+	//FullNames []string `json:"name"`
+	Desc string `json:"desc"`
 }
 
 func main() {
+	if dataDir == "" {
+		dataDir = os.Getenv("HOME") + "/.local/share"
+	}
 	if _, err := exec.LookPath("brew"); err != nil {
 		handleErrStr("Homebrew is not installed. Install it to use Tii")
 		runWithPrompt("Install Homebrew", `/bin/bash -c "\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`)
@@ -75,81 +93,135 @@ func main() {
 		fmt.Println("Tii " + version)
 		return
 	}
+	if hasOption, _ := argsHaveOption("refresh-cache", "r"); hasOption {
+		_, _, err := getCachedPackageInfo(true)
+		if err != nil {
+			handleErr(err)
+			return
+		}
+		fmt.Println("Cache refreshed!")
+		return
+	}
 	if disablePrompts {
 		fmt.Println("Running Tii in non-interactive mode. ($TII_DISABLE_INTERACTIVE is true)")
 	}
 	findPkg(os.Args[1])
 }
 
-// returns true if the package was found and the user installed it
 func findPkg(search string) {
-	core, err := os.Open(prefix + "/homebrew-core/Formula")
+	list, descriptions, err := getCachedPackageInfo(false)
 	if err != nil {
-		handleErrStr("Could not open " + prefix)
 		handleErr(err)
 		return
 	}
-	list, err := core.Readdirnames(0) // >=0 to read all files and folders
-	_ = core.Close()                  // close file right after reading so it's run even if ^C or os.Exit() used later in the function
-	if err != nil {
-		handleErrStr("An error occurred while trying to list files in " + prefix + "/homebrew-core/Formula")
-		handleErr(err)
-		return
-	}
-	cask, err := os.Open(prefix + "/homebrew-cask/Casks")
-	if err != nil {
-		handleErrStr("Could not open " + prefix)
-		handleErr(err)
-		return
-	}
-	casklist, err := cask.Readdirnames(0) // >=0 to read all files and folders
-	_ = cask.Close()
-	if err != nil {
-		handleErrStr("An error occurred while trying to list files in " + prefix + "/homebrew-cask/Casks")
-		handleErr(err)
-		return
-	}
-	list = append(list, casklist...)
+	matches := fuzzy.RankFindFold(search, list)
+	sort.Sort(matches)
 
-	possibleMatches := make([]string, 0, 5)
-	gotExactMatch := false
-	for _, name := range list {
-		formulaName := strings.TrimSuffix(name, filepath.Ext(name))
-		if formulaName == search {
-			fmt.Println("Found exact match: " + color.YellowString(formulaName))
-			gotExactMatch = true
+	if len(matches) > 0 {
+		if matches[0].Target == search {
+			fmt.Println("Found exact match: " + color.YellowString(matches[0].Target) + color.HiBlackString(" ("+descriptions[list[matches[0].OriginalIndex]]+")"))
 			if autoInstallExactMatches {
 				fmt.Println("Installing it because auto-install is enabled. ($TII_AUTO_INSTALL_EXACT_MATCHES is true)")
-				run("brew install " + formulaName)
+				run("brew install " + matches[0].Target)
 				return
 			}
-			if runWithPrompt("Install it", "brew install "+formulaName) {
-				return
-			}
-			break
-		} else if strings.Contains(formulaName, search) || strings.Contains(search, formulaName) {
-			possibleMatches = append(possibleMatches, formulaName)
+			runWithPrompt("Install it", "brew install "+matches[0].Target)
+			return
 		}
-	}
-	if len(possibleMatches) > 0 {
-		fmt.Println("Presenting possible matches [" + color.CyanString(strconv.Itoa(len(possibleMatches))) + "]")
-		for i, name := range possibleMatches {
-			fmt.Println(color.CyanString(strconv.Itoa(i+1)) + ": " + name)
-			if i == 99 {
+
+		fmt.Println("Presenting fuzzy matches")
+		for i, match := range matches {
+			if match.Distance > 10 {
+				break
+			}
+			fmt.Println(color.CyanString(strconv.Itoa(i+1)) + ": " + match.Target + color.HiBlackString(" ("+descriptions[list[match.OriginalIndex]]+")"))
+			if i == 9 {
 				fmt.Println("... and more")
 				break
 			}
 		}
-		if ok, i := promptInt("Enter number to install or press enter to quit", 1, len(possibleMatches)); ok {
-			if runWithPrompt("Install it", "brew install "+possibleMatches[i-1]) {
+		if ok, i := promptInt("Enter number to install or press enter to quit", 1, len(matches)); ok {
+			if runWithPrompt("Install it", "brew install "+matches[i-1].Target) {
 				return
 			}
 		}
 	}
-	if !gotExactMatch {
-		fmt.Println("No exact matches found for " + color.YellowString(search) + ".")
-		runWithPrompt("Update Homebrew formulae database", "brew update")
+	fmt.Println("No exact matches found for " + color.YellowString(search) + ".")
+	runWithPrompt("Update Homebrew formulae database", "brew update")
+}
+
+func getCachedPackageInfo(forceRefresh bool) ([]string, map[string]string, error) {
+	if _, err := os.Stat(dataDir + "/tii/" + dataFile); os.IsNotExist(err) || forceRefresh {
+		err = os.MkdirAll(dataDir+"/tii", 0755)
+		if err != nil {
+			return nil, nil, err
+		}
+		f, err := os.Create(dataDir + "/tii/" + dataFile)
+		if err != nil {
+			return nil, nil, err
+		}
+		defer f.Close()
+		list, descriptions, err := fetchPackageInfo()
+		if err != nil {
+			return nil, nil, err
+		}
+		err = json.NewEncoder(f).Encode(descriptions) // descriptions also has the full list, so no need to save list
+		if err != nil {
+			return nil, nil, err
+		}
+		return list, descriptions, nil
 	}
+	f, err := os.Open(dataDir + "/tii/" + dataFile)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer f.Close()
+	var descriptions map[string]string
+	err = json.NewDecoder(f).Decode(&descriptions)
+	if err != nil {
+		return nil, nil, err
+	}
+	var list []string
+	for name := range descriptions {
+		list = append(list, name)
+	}
+	return list, descriptions, nil
+}
+
+// returns the list, the map to descriptions, and an error
+func fetchPackageInfo() ([]string, map[string]string, error) {
+	var list []string
+	var formulae []Formula
+	var casks []Cask
+	var descriptions = make(map[string]string, 1000)
+
+	resp, err := http.Get(formulaURL)
+	if err != nil {
+		handleErr(err)
+	}
+	defer resp.Body.Close()
+	err = json.NewDecoder(resp.Body).Decode(&formulae)
+	if err != nil {
+		handleErr(err)
+	}
+	resp, err = http.Get(caskURL)
+	if err != nil {
+		handleErr(err)
+	}
+	defer resp.Body.Close()
+	err = json.NewDecoder(resp.Body).Decode(&casks)
+	if err != nil {
+		handleErr(err)
+	}
+	for _, formula := range formulae {
+		list = append(list, formula.Name)
+		descriptions[formula.Name] = formula.Desc
+	}
+	for _, cask := range casks {
+		list = append(list, cask.Name)
+		descriptions[cask.Name] = cask.Desc
+	}
+	return list, descriptions, nil
 }
 
 func promptBool(promptStr string) (yes bool) {
